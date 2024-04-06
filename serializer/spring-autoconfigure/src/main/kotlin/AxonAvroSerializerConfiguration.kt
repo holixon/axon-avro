7639243392b2a820bb726d74e3b976132a7f564e
Simon Zambrovski <simon.zambrovski@holisticon.de>
2024-04-06 21:36:36 +0200

    create schema resource, create http tests, setup docker-compose for local tests

--- serializer/spring-autoconfigure/src/main/kotlin/AxonAvroSerializerConfiguration.kt
+++ serializer/spring-autoconfigure/src/main/kotlin/AxonAvroSerializerConfiguration.kt
@@ -3,14 +3,15 @@ package io.holixon.axon.avro.serializer.spring
 import com.github.avrokotlin.avro4k.Avro
 import io.holixon.axon.avro.serializer.AvroSerializer
 import io.toolisticon.avro.kotlin.AvroSchemaResolver
-import org.axonframework.serialization.Serializer
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 
 /**
  * Configuration for Axon serializer.
  */
+@EnableConfigurationProperties(AxonAvroSerializerProperties::class)
 open class AxonAvroSerializerConfiguration {
   companion object {
     const val EVENT_SERIALIZER = "eventSerializer"
@@ -34,5 +35,7 @@ open class AxonAvroSerializerConfiguration {
 //  fun avroSerializer(builder: AvroSerializer.Builder): Serializer = AvroSerializer(builder)
 
 
-
+  @Bean
+  @ConditionalOnProperty(value = ["\${axon.avro.serializer.rest-enabled}"], havingValue = "true", matchIfMissing = true)
+  fun schemaResolverRestResource(schemaResolver: AvroSchemaResolver) = AvroSchemaResolverResource(schemaResolver = schemaResolver)
 }
